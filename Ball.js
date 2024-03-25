a6bf7b489a2538e4d177284b8d4ba8dff27be221
Scott E Thompson <sthompson082290@gmail.com>
2024-03-25 14:27:18 -0500

    Removed unnecessary comments

--- Ball.js
+++ Ball.js
@@ -1,85 +1,3 @@
-// // export const createBall = (ballElement) => {
-// //     return {ballElement: ballElement};
-// // }
-
-// const INITIAL_VELOCITY = 0.025
-// const VELOCITY_INCREASE = 0.00001
-
-
-// export default class createBall {
-//     constructor(ballElement) {
-//         this.ballElement = ballElement
-//         this.reset()
-//     }
-
-//     get x() {
-//         // Taken value from CSS and converted into a JS # we can use
-//         return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--x"));
-//     }
-
-//     set x(value) {
-//         this.ballElement.style.setProperty("--x", value)
-//     }
-//     get y() {
-//         return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--y"));
-//     }
-
-//     set y(value) {
-//         this.ballElement.style.setProperty("--y", value)
-//     }
-
-//     rect() {
-//         return this.ballElement.getBoundingClientRect();
-//     }
-
-//     reset() {
-//         this.x = 50
-//         this.y = 50
-
-//         //Using a while loop to make sure the ball moves at an optimal pace to the left and right
-//         this.direction = { x: 0 }
-//          // Getting a # between 0 and 2 PI which is essentially 360° given in radians (unit of angle)
-//             // With radians we can use cosine and sine (trig functions of an angle) to determine the x and y direction
-//         while (
-//           Math.abs(this.direction.x) <= 0.2 ||
-//           Math.abs(this.direction.x) >= 0.9
-//         ) {
-//           const heading = randomNumberBetween(0, 2 * Math.PI)
-//           this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
-//         }
-//         this.velocity = INITIAL_VELOCITY
-//       }
-
-//     //This will update the positioning using our getter and setter
-//    update(delta, paddleRects) {
-//     this.x += this.direction.x * this.velocity * delta
-//     this.y += this.direction.y * this.velocity * delta
-//     this.velocity += VELOCITY_INCREASE * delta
-//     const rect = this.rect()
-
-//     if (rect.bottom >= window.innerHeight || rect.top <= 0) {
-//       this.direction.y *= -1
-//     }
-
-//     if (paddleRects.some(r => isCollision(r, rect))) {
-//       this.direction.x *= -1
-//     }
-//   }
-// }
-
-// function randomNumberBetween(min, max) {
-//     return Math.random() * (max - min) + min
-//   }
-  
-//   function isCollision(rect1, rect2) {
-//     return (
-//       rect1.left <= rect2.right &&
-//       rect1.right >= rect2.left &&
-//       rect1.top <= rect2.bottom &&
-//       rect1.bottom >= rect2.top
-//     )
-//   }
-
 const INITIAL_VELOCITY = 0.025
 const VELOCITY_INCREASE = 0.00001
 
@@ -90,6 +8,7 @@ export default class Ball {
   }
 
   get x() {
+    // Taken value from CSS and converted into a JS # we can use
     return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
   }
 
@@ -112,7 +31,12 @@ export default class Ball {
   reset() {
     this.x = 50
     this.y = 50
+
+    //Using a while loop to make sure the ball moves at an optimal pace to the left and right
     this.direction = { x: 0 }
+
+    // Getting a # between 0 and 2 PI which is essentially 360° given in radians (unit of angle)
+    // With radians we can use cosine and sine (trig functions of an angle) to determine the x and y direction
     while (
       Math.abs(this.direction.x) <= 0.2 ||
       Math.abs(this.direction.x) >= 0.9
@@ -123,6 +47,7 @@ export default class Ball {
     this.velocity = INITIAL_VELOCITY
   }
 
+  //This will update the positioning using our getter and setter
   update(delta, paddleRects) {
     this.x += this.direction.x * this.velocity * delta
     this.y += this.direction.y * this.velocity * delta
--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// import createBall  from "./Ball";
-
-// // Selecting the ball element and creating a new class for it
-// const ball = new createBall(document.getElementById('ball'));
-// const playerPaddle = new Paddle(document.getElementById("player-paddle"))
-// const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
-// const playerScoreElem = document.getElementById("player-score")
-// const computerScoreElem = document.getElementById("computer-score")
-
-// let lastTime;
-// // Creating an infinite loop to run the game, called every time the screen is allowed to change
-// function update(time) {
-//     if (lastTime != null) {
-//         //Converting the time passed in into delta
-//         const delta = time - lastTime;
-
-//         //Update code, using delta so all movements are based on it
-//         ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
-//         computerPaddle.update(delta, ball.y)
-//     }
-//     lastTime = time;
-//     window.requestAnimationFrame(update)
-// }
-
-// // Using this instead of setInterval because setInterval isn't always the most accurate,
-// // This way we call the function every time something is ran/changed on the screen
-// window.requestAnimationFrame(update)
-
-
 import Ball from "./Ball.js"
 import Paddle from "./Paddle.js"
 
